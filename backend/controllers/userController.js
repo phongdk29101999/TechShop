61d4739e901945acbf73253cac9d4ffdd9754823
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2021-03-07 05:27:30 +0700

    authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc    Auth user & get token
-// @route   POST /api/user/login
+// @route   POST /api/users/login
 // @access    Public
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
@@ -24,4 +24,23 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-export  { authUser }
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access    Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('Invalid email or password')
+    }
+})
+
+export { authUser, getUserProfile }
