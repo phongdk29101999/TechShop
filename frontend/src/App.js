6e7ac50afea3ed96be44995f6ba19328d8e4e4de
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2021-03-28 05:30:13 +0700

    place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
     return (
@@ -18,8 +19,9 @@ const App = () => {
             <Header className='py-3' />
             <main className='py-3'>
                 <Container>
-                    <Route path='/payment' component={PaymentScreen} />
                     <Route path='/shipping' component={ShippingScreen} />
+                    <Route path='/payment' component={PaymentScreen} />
+                    <Route path='/placeorder' component={PlaceOrderScreen} />
                     <Route path='/login' component={LoginScreen} />
                     <Route path='/register' component={RegisterScreen} />
                     <Route path='/profile' component={ProfileScreen} />
