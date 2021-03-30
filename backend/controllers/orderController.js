6a40db538913c27836b15080f802c8760561beed
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2021-03-31 06:00:17 +0700

    update to paid endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,8 +39,11 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @desc    Get order by Id
 // @route   GET /api/orders/:id
 // @access  Private
-const getOrderById = asyncHandler(async(req, res) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name email')
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
 
     if (order) {
         res.json(order)
@@ -50,4 +53,29 @@ const getOrderById = asyncHandler(async(req, res) => {
     }
 })
 
-export { addOrderItems, getOrderById }
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
