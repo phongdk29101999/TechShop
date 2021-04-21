eaa8e79f44fcca55868723d155f7cd61c384ce36
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2021-04-21 17:12:06 +0700

    admin delete product handler

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import {
     productListReducer,
     productDetailsReducer,
+    productDeleteReducer,
 } from './reducers/productReducers'
 import { cartReducers } from './reducers/cartReducers'
 import {
@@ -25,6 +26,7 @@ import {
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
     cart: cartReducers,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
