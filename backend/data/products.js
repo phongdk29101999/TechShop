38b16e223e1860cf6011b6c5d49f69c74c8c3484
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2021-05-02 06:49:16 +0700

    change sample product review and rating to 0

--- backend/data/products.js
+++ backend/data/products.js
@@ -8,8 +8,8 @@ const products = [
     category: 'Electronics',
     price: 89.99,
     countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'iPhone 11 Pro 256GB Memory',
@@ -20,8 +20,8 @@ const products = [
     category: 'Electronics',
     price: 599.99,
     countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Cannon EOS 80D DSLR Camera',
@@ -32,8 +32,8 @@ const products = [
     category: 'Electronics',
     price: 929.99,
     countInStock: 5,
-    rating: 3,
-    numReviews: 12,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Sony Playstation 4 Pro White Version',
@@ -44,8 +44,8 @@ const products = [
     category: 'Electronics',
     price: 399.99,
     countInStock: 11,
-    rating: 5,
-    numReviews: 12,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Logitech G-Series Gaming Mouse',
@@ -56,8 +56,8 @@ const products = [
     category: 'Electronics',
     price: 49.99,
     countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
+    rating: 0,
+    numReviews: 0,
   },
   {
     name: 'Amazon Echo Dot 3rd Generation',
@@ -68,8 +68,8 @@ const products = [
     category: 'Electronics',
     price: 29.99,
     countInStock: 0,
-    rating: 4,
-    numReviews: 12,
+    rating: 0,
+    numReviews: 0,
   },
 ]
 
