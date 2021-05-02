f9994355fb51d14d74154d72a3e760eed3b65fbd
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2021-05-02 07:17:03 +0700

    fix logout reset data

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -67,54 +67,58 @@ const ProfileScreen = ({ location, history }) => {
       <Col md={3}>
         <h2>User Profile</h2>
         {message && <Message variant='danger'>{message}</Message>}
-        {error && <Message variant='danger'>{error}</Message>}
         {success && <Message variant='success'>Profile Updated</Message>}
-        {loading && <Loader />}
-        <Form onSubmit={submitHandler}>
-          <FormGroup controlId='name'>
-            <FormLabel>Name</FormLabel>
-            <FormControl
-              type='name'
-              placeholder='Enter name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></FormControl>
-          </FormGroup>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danger'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <FormGroup controlId='name'>
+              <FormLabel>Name</FormLabel>
+              <FormControl
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></FormControl>
+            </FormGroup>
 
-          <FormGroup controlId='email'>
-            <FormLabel>Email Address</FormLabel>
-            <FormControl
-              type='email'
-              placeholder='Enter email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></FormControl>
-          </FormGroup>
+            <FormGroup controlId='email'>
+              <FormLabel>Email Address</FormLabel>
+              <FormControl
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></FormControl>
+            </FormGroup>
 
-          <FormGroup controlId='password'>
-            <FormLabel>Password</FormLabel>
-            <FormControl
-              type='password'
-              placeholder='Enter password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></FormControl>
-          </FormGroup>
+            <FormGroup controlId='password'>
+              <FormLabel>Password</FormLabel>
+              <FormControl
+                type='password'
+                placeholder='Enter password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></FormControl>
+            </FormGroup>
 
-          <FormGroup controlId='confirmPassword'>
-            <FormLabel>Confirm Password</FormLabel>
-            <FormControl
-              type='password'
-              placeholder='C onfirm password'
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            ></FormControl>
-          </FormGroup>
+            <FormGroup controlId='confirmPassword'>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl
+                type='password'
+                placeholder='C onfirm password'
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              ></FormControl>
+            </FormGroup>
 
-          <Button type='submit' variant='primary'>
-            Update
-          </Button>
-        </Form>
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        )}
       </Col>
       <Col md={9}>
         <h2>My Orders</h2>
